95e3540899f54f8c014331103a4205bd445e32e3
Alistair Vu <alistairvu@gmail.com>
2021-03-24 22:49:13 +0700

    Finish basic auth api

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,21 @@
+class RegistrationsController < ApplicationController
+  def create
+    user = User.create(registration_params)
+
+    if user
+      session[:user_id] = user.id
+      render json: {
+        status: :created,
+        user: user,
+      }
+    else
+      render json: { status: 500 }
+    end
+  end
+
+  private
+
+  def registration_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+end
